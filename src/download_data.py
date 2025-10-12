a1b0a317759fa45d3e4ed4282eba1dfc3181e863
Liyari <alliahmarisorono@gmail.com>
2025-10-12 15:17:32 +0800

    Improved set up environment for data

--- src/download_data.py
+++ src/download_data.py
@@ -1,15 +1,53 @@
 import os
 import zipfile
+import shutil
+from pathlib import Path
 
-# Dataset: https://www.kaggle.com/datasets/asdasdasasdas/garbage-classification
-dataset = "asdasdasasdas/garbage-classification"
-raw_dir = "data/raw"
+#Kaggle 
+dataset = "sumn2u/garbage-classification-v2"
+raw_dir = Path("data/raw")
 
-# Create folder if not exists
-os.makedirs(raw_dir, exist_ok=True)
+raw_dir.mkdir(parents=True, exist_ok=True)
 
-# Download dataset (with --unzip to avoid manual extraction)
+#Download dataset
 cmd = f"kaggle datasets download -d {dataset} -p {raw_dir} --unzip"
 os.system(cmd)
 
-print("✅ Dataset downloaded and extracted to", raw_dir)
+#Flattens 2 versions of kaggle dataset v1 and v2 incase u made a mistake (Like me :p)
+possible_folders = [
+    raw_dir / "Garbage classification",
+    raw_dir / "Garbage classification v2",
+]
+
+for folder in possible_folders:
+    if folder.exists():
+        print(f"📂 Found {folder.name}")
+        for item in folder.iterdir():
+            dest = raw_dir / item.name
+            if dest.exists():
+                print(f"⚠️ Skipping {item.name} (already exists)")
+                continue
+            shutil.move(str(item), raw_dir)
+        shutil.rmtree(folder, ignore_errors=True)
+
+# remove leftover zips
+for z in raw_dir.glob("*.zip"):
+    z.unlink()
+
+# organize folders
+expected_classes = [
+    "battery", "biological", "cardboard", "clothes",
+    "glass", "metal", "paper", "plastic", "shoes", "trash"
+]
+
+# Move loose images into correct class folders
+for img in raw_dir.glob("*.jpg"):
+    label = img.stem.split("_")[0].lower()
+    if label in expected_classes:
+        dest = raw_dir / label
+        dest.mkdir(exist_ok=True)
+        shutil.move(str(img), dest / img.name)
+
+found_classes = [d.name for d in raw_dir.iterdir() if d.is_dir()]
+print("\n✅ Dataset organized under data/raw/")
+print("Found categories:", found_classes)
